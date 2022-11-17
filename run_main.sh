6d863d2760d03cca9151f0581e90664e6d6a728b
joobumjin <bumjin.joo@gmail.com>
2022-11-17 16:27:36 -0500

    feat: fixing sh args

--- run_main.sh
+++ run_main.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Request a GPU partition node and access to 1 GPU
-#SBATCH --partition=gpu --gpus=2
+#SBATCH --partition=gpu --gres=gpu:1 
 
 # Request 1 CPU core
-#SBATCH --nodes=4
+#SBATCH -n 4
 #SBATCH -t 04:00:00
 #SBATCH --mem=128g
 
