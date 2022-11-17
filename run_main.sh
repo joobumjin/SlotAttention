4f696f4a71076d74eb6e78a1555a1d3bde3b5f24
joobumjin <bumjin.joo@gmail.com>
2022-11-17 17:54:03 -0500

    config

--- run_main.sh
+++ run_main.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# Request a GPU partition node and access to 1 GPU
-#SBATCH --partition=gpu --gres=gpu:1 
+#SBATCH --partition=gpu --gres=gpu:2
 
-# Request 1 CPU core
 #SBATCH -n 4
-#SBATCH -t 04:00:00
+#SBATCH -t 02:00:00
 #SBATCH --mem=128g
 
 # Load a CUDA module
